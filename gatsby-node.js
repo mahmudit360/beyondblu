bf75b44922a69013a99d4a65f74e0debcf4a5206
mahmudit360 <167845700+mahmudit360@users.noreply.github.com>
2024-04-24 12:08:20 +0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,56 +2,56 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // NOTE: Commented out as not currently using a blog
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
 
-//   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-//   const result = await graphql(
-//     `
-//       {
-//         allMarkdownRemark(
-//           sort: { fields: [frontmatter___date], order: DESC }
-//           limit: 1000
-//         ) {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//                 langKey
-//               }
-//               frontmatter {
-//                 title
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-//   )
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+                langKey
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  )
 
-//   if (result.errors) {
-//     throw result.errors
-//   }
+  if (result.errors) {
+    throw result.errors
+  }
 
-//   // Create blog posts pages.
-//   const posts = result.data.allMarkdownRemark.edges
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges
 
-//   posts.forEach((post, index) => {
-//     const previous = index === posts.length - 1 ? null : posts[index + 1].node
-//     const next = index === 0 ? null : posts[index - 1].node
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
 
-//     createPage({
-//       path: post.node.fields.slug,
-//       component: blogPost,
-//       context: {
-//         slug: post.node.fields.slug,
-//         langKey: post.node.fields.langKey,
-//         previous,
-//         next,
-//       },
-//     })
-//   })
-// }
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        langKey: post.node.fields.langKey,
+        previous,
+        next,
+      },
+    })
+  })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
