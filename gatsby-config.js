2ae6724c7537da2e110074b3bccbe1feb0003d66
mahmudit360 <167845700+mahmudit360@users.noreply.github.com>
2024-04-24 12:07:43 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
     },
   },
   plugins: [
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `${__dirname}/content/blog`,
-    //     name: `blog`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-netlify`,
