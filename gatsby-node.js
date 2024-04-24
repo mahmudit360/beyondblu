a47929ff626452b70bb51fedeba1aa415f8965ad
mahmudit360 <167845700+mahmudit360@users.noreply.github.com>
2024-04-24 06:53:13 +0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,71 +1,48 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+  const result = await graphql(`
+    {
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        edges {
+          node {
+            fields {
+              slug
+              langKey
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
 
-// NOTE: Commented out as not currently using a blog
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-
-//   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-//   const result = await graphql(
-//     `
-//       {
-//         allMarkdownRemark(
-//           sort: { fields: [frontmatter___date], order: DESC }
-//           limit: 1000
-//         ) {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//                 langKey
-//               }
-//               frontmatter {
-//                 title
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-//   )
-
-//   if (result.errors) {
-//     throw result.errors
-//   }
-
-//   // Create blog posts pages.
-//   const posts = result.data.allMarkdownRemark.edges
-
-//   posts.forEach((post, index) => {
-//     const previous = index === posts.length - 1 ? null : posts[index + 1].node
-//     const next = index === 0 ? null : posts[index - 1].node
-
-//     createPage({
-//       path: post.node.fields.slug,
-//       component: blogPost,
-//       context: {
-//         slug: post.node.fields.slug,
-//         langKey: post.node.fields.langKey,
-//         previous,
-//         next,
-//       },
-//     })
-//   })
-// }
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  // SOURCE: https://github.com/gaearon/overreacted.io/commit/18c7308
-  if (
-    node.internal.type === `MarkdownRemark` &&
-    node.internal.fieldOwners.slug !== "gatsby-plugin-i18n"
-  ) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
+  if (result.errors) {
+    throw result.errors
   }
+
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: post.node.fields.slug,
+        langKey: post.node.fields.langKey,
+        previous,
+        next,
+      },
+    })
+  })
+
+  // Optionally, create a blog archive page
+  createPage({
+    path: '/blog',
+    component: path.resolve(`./src/pages/blog.js`) // Ensure you have a blog.js file
+  })
 }
